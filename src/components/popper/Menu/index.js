b97be603fb2ab1aa91bb24ccdbace8ad07a47da9
linhnguyenvan220@gmail.com <linhnguyenvan220@gmail.com>
2024-10-25 23:07:33 +0700

    Xây dựng UI phần Header #5

--- src/components/popper/Menu/index.js
+++ src/components/popper/Menu/index.js
@@ -1,24 +1,56 @@
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
+
 import { Wrapper as PopperWrapper } from '~/components/popper';
 import MenuItem from './MenuItem';
+import Header from './Header';
 import styles from './Menu.module.scss';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
-function Menu({ children, items = [] }) {
+function Menu({ children, items = [], onchange }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
     const renderItems = () => {
-        return items.map((item, index) => <MenuItem key={index} data={item} />);
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onchange(item);
+                        }
+                    }}
+                />
+            );
+        });
     };
 
     return (
         <Tippy
+            // visible
             interactive
-            delay={[0,800]}
+            delay={[0, 800]}
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && (
+                            <Header
+                                title="Language"
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                            ></Header>
+                        )}
+                        {renderItems()}
+                    </PopperWrapper>
                 </div>
             )}
         >
